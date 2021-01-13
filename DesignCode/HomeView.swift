010aae9b9630578cfad72c3694d18f233febde20
erenlk <elik.dark91@icloud.com>
2021-01-13 20:58:30 +0300

    15

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -5,6 +5,12 @@
 //  Created by Eren  Çelik on 11.01.2021.
 //
 
+
+
+
+
+
+
 import SwiftUI
 
 struct HomeView: View {
@@ -19,8 +25,26 @@ struct HomeView: View {
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
-            .padding(.top,30)
+            .padding(.leading, 14)
+            .padding(.top, 30)
             
+            ScrollView(.horizontal,showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(sectionData) { item in
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(
+                                    .degrees(Double(geometry.frame(in: .global).minX - 30) / -20
+                                    ),
+                                    axis: (x: 0, y: 10, z: 0.0)
+                                )
+                        }
+                        .frame(width : 275 , height : 275)
+                    }
+                }
+                .padding(30)
+                .padding(.bottom,30)
+            }
             Spacer()
         }
     }
@@ -31,3 +55,52 @@ struct HomeView_Previews: PreviewProvider {
         HomeView(showProfile: .constant(false))
     }
 }
+
+struct SectionView: View {
+    
+    var section: Section
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top){
+                Text(section.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image(section.logo)
+            }
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity,alignment: .leading)
+            
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+            
+        }
+        .padding(.top, 20)
+        .padding(.horizontal , 20)
+        .frame(width: 275,height: 275)
+        .background(section.color)
+        .cornerRadius(30)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
+    }
+}
+
+struct Section: Identifiable{
+    var id = UUID()
+    var title : String
+    var text  : String
+    var logo  : String
+    var image : Image
+    var color : Color
+}
+
+let sectionData =
+    [
+        Section(title: "Prototypr Design in SwiftUI", text: "18 Section", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+        Section(title: "Build Swift UI App", text: "20 Section", logo: "Logo1", image: Image("Card2"), color: Color("card2")),
+        Section(title: "Swift UI Advance", text: "20 Section", logo: "Logo1", image: Image("Card3"), color: Color("card3"))
+    ]
+
